9edc1221a0b26f5d6ec57ef87448e13e8c482dd9
Ralph Lantin <ralph.lantin@gmail.com>
2021-06-18 00:00:14 +0800

    updated cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_LvQz9kpAt', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '16a02er9qfduoaodm0nuu5qne4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
